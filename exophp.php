08ac84c21d1eed70fae5ca69f552c646d22ea035
florent ferlay <florent.ferlay@neuf.fr>
2018-07-23 14:12:51 +0200

    oui

---
+++ exophp.php
@@ -0,0 +1,64 @@
+<?php
+/* 1.*/
+
+$name = "florent";
+
+/*2.*/
+
+$lastname=  "FERLAY";
+$firstname= "florent";
+$age= 28;
+
+echo $lastname. " ". $firstname." " . $age." " ."<br>";
+
+/*3.*/
+
+$km = 1;
+echo  $km . " " ;
+
+$km = 3;
+echo " " .$km . " " ;
+
+$km = 125;
+echo " " .$km . " " ."<br>";
+
+/*4.*/
+
+
+$string = "bip";
+$int = 2;
+$float = 2.8; 
+$booléan = false; 
+
+echo $string ." ". $int ." ". $float ." ". $booléan . "<br>";
+
+/*5.*/
+
+$int = NULL;
+echo $int;
+$int = 43;
+echo $int ."<br>";
+
+/*6*/
+
+$name = "Maurice";
+echo "Bonjour ".$name . " ,comment vas tu ?<br>";
+
+
+
+/*7.*/
+
+$lastname = "Bogdanov";
+$firstname = "Grichka";
+$age = 60;
+
+
+echo "Bonjour " .$lastname." ".$firstname." ,tu as " . $age . " ans <br>" ;
+
+/*8.*/
+
+$result1 = 3 + 4;
+$result2 = 5 * 20;
+$result3 = 45 / 5;
+
+echo $result1 . " " .$result2 . " " .$result3;
